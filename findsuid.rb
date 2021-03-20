38215aa8eee854e2352a94b2a00bd58ea7f67f9a
ironzombi <drgrep@ironzombi.com>
2021-03-20 09:43:58 +0800

    find suid fix

--- findsuid.rb
+++ findsuid.rb
@@ -7,7 +7,7 @@ def finds(path)
     files = []
     Find.find(path) do |f|
         if (File.file?(f) && File.stat(f).setuid? && File.executable?(f))
-            files << file
+            files << f
         end
     rescue
         next
